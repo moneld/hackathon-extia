451c0ff3b84139ce789586347af0e7e7467ba77d
Jean <oka.jeanjaures@gmail.com>
2023-03-01 20:15:10 +0100

    participant model and route adding 1

---
+++ controllers/user.js
@@ -0,0 +1,61 @@
+const User = require('../models/user');
+const bcrypt = require('bcryptjs');
+
+//todo : display json message into postman after request launching
+//todo : signup with account checking  / connexion / disconnection
+//todo : send mail
+//todo : validate email | npm install express-validator
+
+
+exports.getAllUser= async(req, res, next) => {
+    const users = await User.find()
+        .then(users => res.status(200).json(users))
+        .catch(error => res.status(501).json({error}));
+}
+
+exports.deleteUser=async(req, res, next) => {
+    console.log("delete user");
+    await User.deleteOne({id:req.params.id})
+        .then(() => res.status(204).json({ message: 'user deleted !'}))
+        .catch(error => res.status(501).json({ error }));
+}
+
+exports.createUser=async(req, res, next) => {
+    console.log("create user");
+    const user = await new User(req.body);
+    user.save()
+        .then(() => res.status(201).json({ message: 'user created !'}))
+        .catch(error => res.status(400).json({ error }));
+}
+
+exports.getUserById=async(req, res) => {
+    console.log("get user by id");
+    try{
+        let user=await User.findOne({id:req.params.id});
+        if (user){
+            return res.status(200).json(user);
+        }
+        res.status(404).json({ message: 'user not found !'})
+    } catch(error){
+        res.status(501).json({ message : error.message })
+    }
+}
+
+exports.updateUserById= async(req, res) => {
+    console.log("update user by id");
+    const user=await User.updateOne({id:req.params.id}, req.body)
+        .then(()=> res.status(201).json({ message: 'user up-to-date  !'}))
+        .catch(error => res.status(501).json({ error }));
+}
+exports.updatePartUserById=async(req, res) => {
+
+
+    const user=await User.findByIdAndUpdate(req.params.id, req.body, {new: true}).then((user) => {
+        if (!user) {
+            return res.status(404).send();
+        }
+        res.send(user);
+    }).catch((error) => {
+        res.status(501).json({message : error.message});
+    })
+}
---
+++ routes/user.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const router = express.Router();
+
+const UserCtrl = require('../controllers/user');
+
+router.get('/all',UserCtrl.getAllUser);
+router.delete('/delete/:id',UserCtrl.deleteUser);
+router.get('/:id',UserCtrl.getUserById);
+router.put('/update/:id',UserCtrl.updateUserById);
+router.patch('/update-part/:id',UserCtrl.updateUserById);
+module.exports = router;
