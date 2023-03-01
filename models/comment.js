516da8b6ec70ae5c8a7fbfcaa44a192d60d233e0
Jean <oka.jeanjaures@gmail.com>
2023-03-01 19:55:19 +0100

    participant model and route adding

---
+++ models/comment.js
@@ -0,0 +1,8 @@
+const mongoose=require('mongoose');
+
+const Schema = mongoose.Schema;
+const commentSchema=new Schema({
+    note:{type : Number,required: true},
+    content:{type : String , required: true},
+});
+module.exports=mongoose.model('Participant',commentSchema);
---
+++ models/participant.js
@@ -0,0 +1,11 @@
+const mongoose=require('mongoose');
+
+const Schema = mongoose.Schema;
+const participantSchema=new Schema({
+    name:{type : String,required:true},
+    firstname:{type : String,required:true},
+    email:{type : String,required:true},
+    login:{type : String,required: true},
+    password:{type : String , required: true},
+});
+module.exports=mongoose.model('Participant',participantSchema);
