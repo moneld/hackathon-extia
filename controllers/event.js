0c7e218455fc5058e1ae6ab8afa417931595b155
Jean <oka.jeanjaures@gmail.com>
2023-03-03 08:47:21 +0100

    models- routes-controllers processing - document field(event) updating 1

---
+++ controllers/event.js
@@ -0,0 +1,18 @@
+const Event = require('../models/event');
+
+//todo: liste des utilisateurs ayant participe au meet
+
+exports.createEvent=async(req, res, next) => {
+    console.log(req.body);
+    const event = await new Event(req.body);
+    event.save()
+        .then(() => res.status(201).json({ message: 'event created !'}))
+        .catch(error => res.status(400).json({ error }));
+
+}
+exports.getAllEvent= async(req, res, next) => {
+
+    const events = await Event.find()
+        .then(events => res.status(200).json(events))
+        .catch(error => res.status(501).json({error}));
+}
---
+++ models/event.js
@@ -0,0 +1,16 @@
+const mongoose=require('mongoose');
+
+
+const Schema = mongoose.Schema;
+const eventSchema=new Schema({
+    tag: {type : String , required: true},
+    description: {type : String , required: true},
+    date:{type : Date , required: true},
+    address:{
+        street:{type : String , required: true},
+        house:{type : String , required: true},
+        city:{type : String , required: true},
+    }
+
+});
+module.exports=mongoose.model('Event',eventSchema);
