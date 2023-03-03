eb37393ae30cf30598094e08acc69e4e172cb16c
Jean <oka.jeanjaures@gmail.com>
2023-03-03 09:16:56 +0100

    models- routes-controllers processing - document field(event) updating 1

--- controllers/meet.js
+++ controllers/meet.js
@@ -1,1 +1,23 @@
-const Comment = require('../models/meet');
+const Meet = require('../models/meet');
+
+//todo: liste des utilisateurs ayant participe au meet
+exports.createMeet=async(req, res, next) => {
+
+    console.log(req.body);
+    const meet = await new Meet(req.body);
+    meet.save()
+        .then(() => res.status(201).json({ message: 'meet created !'}))
+        .catch(error => res.status(400).json({ error }));
+
+}
+exports.getAllMeet= async(req, res, next) => {
+
+    const meets = await Meet.find()
+        .then(meets => res.status(200).json(meets))
+        .catch(error => res.status(501).json({error}));
+
+}
+
+
+
+
--- routes/meet.js
+++ routes/meet.js
@@ -3,11 +3,7 @@ const router = express.Router();
 
 const meetCtrl = require('../controllers/meet');
 
-/*
-router.post('/create',companyCtrl.createCompany);
-router.get('/all',companyCtrl.getAllCompany);
-router.delete('/delete/:id',companyCtrl.deleteCompany);
-router.get('/:id',companyCtrl.getCompanyById);
-router.put('/update/:id',companyCtrl.updateCompanyById);
-router.patch('/update-part/:id',companyCtrl.updateCompanyById);*/
+router.post('/create',meetCtrl.createMeet);
+router.get('/all',meetCtrl.getAllMeet);
+
 module.exports = router;
